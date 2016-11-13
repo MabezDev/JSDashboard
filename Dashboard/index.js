c852d676c7ed7417d4ac7ce59622938f6ecdb11d
Mabez <mabez123@gmail.com>
2016-11-13 14:57:55 +0000

    Restructured code base for node

---
+++ Dashboard/index.js
@@ -0,0 +1,35 @@
+/*jslint node: true */
+
+'use strict';
+
+
+
+function addWidgit(title) {
+    var grid = document.getElementById('widget_grid'),
+        container = document.createElement('div'),
+        titleEl = document.createElement('p');
+    
+    container.className = "widget_item";
+    titleEl.textContent = title;
+    container.appendChild(titleEl);
+    grid.appendChild(container);
+}
+
+
+
+
+
+function init() {
+   // on load set up components
+    console.log("Dashboard loading...");
+    
+    for (var i = 0; i < 5; i++) {
+        addWidgit("Testerino"+(i+1));
+    }
+}
+
+if (typeof(window) !== 'undefined') {
+    var timer = setTimeout(function() {
+        window.addEventListener("load", init);
+    }, 200);
+}

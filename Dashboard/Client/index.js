a24dd571eb57fc2163240c46f5113bf7431de3a1
Mabez <mabez123@gmail.com>
2017-01-31 16:05:12 +0000

    Started working on drag and drop functionality

--- Dashboard/Client/index.js
+++ Dashboard/Client/index.js
@@ -12,6 +12,10 @@ function addWidgit(title) {
     container.className = "widget_item";
     titleEl.textContent = title;
     container.appendChild(titleEl);
+    container.draggable = true; // enable drag and drop in widgets
+    container.ondragstart= dragstart_handler;
+    container.ondragenter = drop_handler;
+    container.ondragover = dragover_handler;
     grid.appendChild(container);
 }
 
@@ -22,10 +26,32 @@ function addWidgit(title) {
 function init() {
    // on load set up components
     console.log("Dashboard loading...");
-    
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 9; i++) {
         addWidgit("Testerino"+(i+1));
     }
+}
+
+function dragstart_handler(event){
+    console.log("Moved!");
+    event.dataTransfer.setData("text/plain", event.target.id);  // wont work as id's are all the same
+    //event.dataTransfer.dropEffect = "copy";
+}
+
+function dragover_handler(ev) {
+ ev.preventDefault();
+ // Set the dropEffect to move
+ ev.dataTransfer.dropEffect = "move"
+}
+function drop_handler(ev) {
+ ev.preventDefault();
+ // Get the id of the target and add the moved element to the target's DOM
+ var data = ev.dataTransfer.getData("text");
+ var elementDropped = document.getElementById(data);
+ var elementDroppedOn = ev.target;
+ //console.log("Dropped text: "+elementDropped.p.textContent);
+ //console.log("DroppedOn text: "+elementDroppedOn.p.textContent);
+ //console.log(elementDropped);
+ console.log(elementDroppedOn);
 }
 
 
--- Dashboard/server.js
+++ Dashboard/server.js
@@ -4,48 +4,12 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var express = require('express');
+var app = express();
 
-var validPaths = ["/index.html","/main.css","/index.js"];
-var path = "Client"
+var webpages = __dirname + "/Client/";
 
+// static files
+app.use('/', express.static(webpages, { extensions: ['html'] }));
 
-var server = http.createServer(handleRequest);
-server.listen(8080);
-
-function handleRequest(req, res){
-    var parsedUrl = url.parse(req.url, true);
-    console.log(req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var type = "text/plain";
-    if(req.url.indexOf('.html') != -1){
-        type = "text/html";
-    } else if(req.url.indexOf('.js') != -1) {
-        type = "text/javascript";
-    } else if(req.url.indexOf('.css') != -1){
-        type = "text/css";
-    }
-    
-    fs.readFile(path + req.url, function (err, data) {
-        if (err){
-            handleError(res);
-        } else {
-            res.writeHead(200, {'Content-Type': type});
-            res.write(data);
-            res.end();
-        }
-    });
-}
-
-function handleError(response){
-    response.statusCode = 404;
-    response.end('Not found!\n');
-}
-
-//function validPath(str){
-//    for (var elm of validPaths) {
-//        if(str === elm){
-//            return true;
-//        }
-//    }
-//    return false;
-//}
+app.listen(8080);
